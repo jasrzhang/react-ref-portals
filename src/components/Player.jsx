a8234bbbcf26529de37998ccea706811a21b8a6a
jasrzhang <jasrzhang@gmail.com>
2024-08-21 22:06:03 +0930

    Practice Ref and Portal with a timer game

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function Player() {
+  const playerNameRef = useRef("unknown entity");
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  const handleClick = () => {
+    setEnteredPlayerName(playerNameRef.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unkown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameRef} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
